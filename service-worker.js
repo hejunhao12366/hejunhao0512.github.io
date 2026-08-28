e3b1bfbe630a872f475e3d05a8eebded60538c2f
hejunhao12366 <3342656208@qq.com>
2026-08-28 23:49:52 +0800

    Bump SW cache to v71 (missed in previous commit)

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const cacheName = "mobile-ledger-v70";
+const cacheName = "mobile-ledger-v71";
 const assets = [
   "./",
   "./index.html",
-  "./styles.css?v=70",
-  "./app.js?v=70",
-  "./drawing.js?v=70",
+  "./styles.css?v=71",
+  "./app.js?v=71",
+  "./drawing.js?v=71",
   "./manifest.webmanifest",
   "./icon.svg",
 ];
